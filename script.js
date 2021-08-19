365d0483eb55c227b91922ccf343748377396abc
Arslan Jan Soomro <arslanjs.dev@gmail.com>
2021-08-19 12:03:11 -0700

    Fetch Data from api, create and insert project card into DOM

--- script.js
+++ script.js
@@ -1,51 +1,66 @@
-import { USERNAME, AUTHHEADER, INCFILENAME } from "./credentials.js";
+import { USERNAME, AUTHHEADER, INCFILENAME, IMGNAME } from "./credentials.js";
+let PROJECTS = [];
 
-//Projects-container slider logic
-const slider = document.querySelector('.projects-container');
+//Projects-container projectSlider logic
+const projectSlider = document.querySelector('.projects-container');
 let isDown = false;
 let startX;
 let scrollLeft;
 
 //The Scroll Animation of Projects container Slider
-const sliderObserver = new IntersectionObserver((entries) => {
+const projectSliderObserver = new IntersectionObserver((entries) => {
     if(entries[0].isIntersecting){
         
-        slider.scrollLeft = slider.offsetWidth;
+        projectSlider.scrollLeft = projectSlider.offsetWidth;
 
-        setTimeout(() => slider.scrollLeft = 0, 1000);
+        setTimeout(() => projectSlider.scrollLeft = 0, 1000);
     }
 }, {threshold: [1]});
-sliderObserver.observe(slider);
+projectSliderObserver.observe(projectSlider);
 
 
-slider.addEventListener('mousedown', (e) => {
+projectSlider.addEventListener('mousedown', (e) => {
     isDown = true;
 
     //To properly scroll when clicking
-    slider.classList.remove("scroll-smooth");
-    sliderObserver.disconnect();
+    projectSlider.classList.remove("scroll-smooth");
+    projectSliderObserver.disconnect();
 
-    startX = e.pageX - slider.offsetLeft;
-    scrollLeft = slider.scrollLeft;
+    startX = e.pageX - projectSlider.offsetLeft;
+    scrollLeft = projectSlider.scrollLeft;
 });
 
-slider.addEventListener('mouseleave', () => {
+projectSlider.addEventListener('mouseleave', () => {
     isDown = false;
 });
 
-slider.addEventListener('mouseup', () => {
+projectSlider.addEventListener('mouseup', () => {
     isDown = false;
 });
 
-slider.addEventListener('mousemove', (e) => {
+projectSlider.addEventListener('mousemove', (e) => {
     if (!isDown) return;
     e.preventDefault();
-    const x = e.pageX - slider.offsetLeft;
+    const x = e.pageX - projectSlider.offsetLeft;
     const walk = (x - startX) * 3; //scroll-fast
-    slider.scrollLeft = scrollLeft - walk;
+    projectSlider.scrollLeft = scrollLeft - walk;
 });
 
 //Api Calls
+
+const filterOneRepo = item => {
+    return {
+        name: item.name,
+        description: item.description,
+        url: item.html_url,
+        pagelink: item.homepage, 
+    }    
+}
+
+const filterRepoProps = rawData => {
+    const newData = rawData.map(filterOneRepo);
+    return newData;
+}
 
 const getAllRepos = async () => {
 
@@ -59,56 +74,88 @@ const getAllRepos = async () => {
     return data;
 }
 
-const filterOneRepo = item => {
-    return {
-        name: item.name,
-        description: item.description,
-        url: item.html_url,
-        pagelink: item.homepage, 
-    }    
+const getContents = async (files) => {
+
+    let contents = {
+        rating: 0,
+        imgsrc: ""
+    };
+
+    for(let i = 0; i < files.length; i++){
+        if(files[i].name == `${INCFILENAME}.json`){
+            let res = await fetch(files[i].download_url);
+            let data = await res.json();
+
+            //All of the information to be carried from $$$INCLUDEPROJECT$$$.json file must be put in contents object here
+            contents.rating = data.RATING;
+        }
+
+        if(files[i].name == `${IMGNAME}.png`){
+            //See which property mathces img src and put it in contents object here
+            contents.imgsrc = "";
+        }
+    }
+
+    return contents;
 }
 
-const filterRepoProps = rawData => {
-    const newData = rawData.map(filterOneRepo);
+const createProjectCard = (info) => {
+    
+    let card = document.createElement('div');
+    card.classList.add('project-card');
 
-    return newData;
+    card.innerHTML = 
+    `
+    <img src=${info.imgsrc} alt=""/>
+    <div class="project-card-disc">
+        <h2>${info.name}</h2>
+        <p>${info.description}</p>
+        <div>
+            <button class="round-btn">More</button>
+            <button class="round-btn">Preview</button>
+        </div>
+    </div>
+    `
+    return card;
 }
 
-
-const getIncludedRepos = async (repos) => {
+const formProjects = async (repos) => {
     
     console.log("Included");
 
     let res;
     let data;
-    let repArr;
+    let repoInfo;
 
     for(let i = 0; i < repos.length; i++){
+        //Fetch Content Of Every Repo And Check If It Will Be Included as a project, it is a project if it contains $$$INCLUDEPROJECT$$$ Folder
         res = await fetch(`https://api.github.com/repos/${USERNAME}/${repos[i].name}/contents/${INCFILENAME}`, AUTHHEADER);
-        data = await res.json();
 
-        if(data && data.name){
-            console.log(i + " " + data.name);
+        if(res.status >= 200 && res.status < 300){
+            data = await res.json();
+            //FETCH IMPORTANT CONTENTS
+            let contents = await getContents(data);
+            //CLEAR THE UNNECESARY DATA OF REPO
+            repoInfo = filterOneRepo(repos[i]);
+            //MERGE REPO AND CONTENTS DATA
+            repoInfo = { ...repoInfo, ...contents};
+            //PUT MERGED DATA INTO PROJECTS Array
+            PROJECTS.push(repoInfo); 
+            //Get Rid Of Loader
+            if(document.querySelector(".projects-container .loader-spinner")){
+                projectSlider.removeChild(document.querySelector(".projects-container .loader-spinner"));
+            }
+            //Inject Into DOM
+            projectSlider.appendChild(createProjectCard(repoInfo));
         }else{
-            console.log(i + " Empty");
+
         }
     }
+
+    //ADD A Sorting FUNCTION HERE
+
+    //ADD IMAGES IN GIT REPOS AND SETUP HERE TO RECEIVE THEM PROPERLY
 }
 
-const getContents = async () => {
-    
-    const res = await fetch(`https://api.github.com/repos/arslan-soomro/qaj/contents/`, AUTHHEADER);
-    const data = await res.json();
+//getAllRepos().then(data => formProjects(data)).catch(err => console.log("Couldn't Load Data", err));
 
-    console.log(data);
-}
-
-//getAllRepos();
-
-//getContents();
-
-//getAllRepos().then(data => getIncludedRepos(data));
-
-// fetch("https://raw.githubusercontent.com/Arslan-Soomro/Login-App/main/%24%24%24INCLUDEPROJECT%24%24%24.json")
-//     .then(res => res.json())
-//     .then(data => console.log(data));
